360e0ca8e1daa94de7c5bef400d4d499847014a0
MatiasPenalozaJara <mat.penaloza@duocuc.cl>
2024-10-08 21:32:55 -0300

    Funciona carrusel aunque problemas con footer y detalles más

--- Frontend/SNZ-Digital/src/app/Componet/catalogo/catalogo.component.ts
+++ Frontend/SNZ-Digital/src/app/Componet/catalogo/catalogo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swiper from 'swiper';
 
 @Component({
   selector: 'app-catalogo',
@@ -9,7 +10,24 @@ export class CatalogoComponent  implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    const swiper = new Swiper('.swiper-container', {
+      loop: true,
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+    });
+  }
+  
 
   // Función para hacer scroll suave a la sección con el id proporcionado
   scrollToSection(sectionId: string) {
