27ee459057bd5ff2dfa3098855f4ba6cbad1ed29
NodyDuoc <101402582+NodyDuoc@users.noreply.github.com>
2024-10-20 16:38:59 -0300

    Revertir Kardex incorrecto

--- Backend/SNZDigital/src/main/java/com/snzDigital/SNZDigital/persistence/repositories/KardexRepository.java
+++
@@ -1,7 +0,0 @@
-package com.snzDigital.SNZDigital.persistence.repositories;
-
-import com.snzDigital.SNZDigital.persistence.entity.KardexEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface KardexRepository extends JpaRepository<KardexEntity, Integer> {
-}
--- Backend/SNZDigital/src/main/java/com/snzDigital/SNZDigital/persistence/repositories/MovimientoKarRepository.java
+++
@@ -1,7 +0,0 @@
-package com.snzDigital.SNZDigital.persistence.repositories;
-
-import com.snzDigital.SNZDigital.persistence.entity.MovimientoKarEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovimientoKarRepository extends JpaRepository<MovimientoKarEntity, Integer> {
-}
