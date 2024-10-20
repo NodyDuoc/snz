6a8e9748f070add6441deb5b921938590c5c8e31
NodyDuoc <101402582+NodyDuoc@users.noreply.github.com>
2024-10-20 16:14:11 -0300

    Movimiento kardex

---
+++ Backend/SNZDigital/src/main/java/com/snzDigital/SNZDigital/persistence/entity/MovimientoKarEntity.java
@@ -0,0 +1,34 @@
+package com.snzDigital.SNZDigital.persistence.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "MOVIMIENTO_KAR")
+public class MovimientoKarEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "mov_seq")
+    @SequenceGenerator(name = "mov_seq", sequenceName = "SNZ1.MOV_SEQ", allocationSize = 1)
+    @Column(name = "MOVID")
+    private Integer movId;
+
+    @Column(name = "MOVNAME", length = 30)
+    private String movName;
+
+    // Getters and Setters
+    public Integer getMovId() {
+        return movId;
+    }
+
+    public void setMovId(Integer movId) {
+        this.movId = movId;
+    }
+
+    public String getMovName() {
+        return movName;
+    }
+
+    public void setMovName(String movName) {
+        this.movName = movName;
+    }
+}
