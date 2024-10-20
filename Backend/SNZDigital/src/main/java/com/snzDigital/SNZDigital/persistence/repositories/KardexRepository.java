8545bf8c0138bb3ae826ec5c40c77b649e8124e5
NodyDuoc <101402582+NodyDuoc@users.noreply.github.com>
2024-10-20 16:57:12 -0300

    Kardex Correcto

---
+++ Backend/SNZDigital/src/main/java/com/snzDigital/SNZDigital/persistence/repositories/KardexRepository.java
@@ -0,0 +1,7 @@
+package com.snzDigital.SNZDigital.persistence.repositories;
+
+import com.snzDigital.SNZDigital.persistence.entity.KardexEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface KardexRepository extends JpaRepository<KardexEntity, Integer> {
+}
