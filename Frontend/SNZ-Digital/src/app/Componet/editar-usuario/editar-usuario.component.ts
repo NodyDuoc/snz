2323492f1e99d338620392335e1b3cd7e4916d30
NodyDuoc <101402582+NodyDuoc@users.noreply.github.com>
2024-10-23 17:51:19 -0300

    Avance editar usuario

--- Frontend/SNZ-Digital/src/app/Componet/editar-usuario/editar-usuario.component.ts
+++ Frontend/SNZ-Digital/src/app/Componet/editar-usuario/editar-usuario.component.ts
@@ -1,79 +1,218 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from 'src/app/Service/auth.service';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthCreateUserRequest } from 'src/models/usuarioI';
+import { AlertController, LoadingController } from '@ionic/angular';
 @Component({
   selector: 'app-editar-usuario',
   templateUrl: './editar-usuario.component.html',
   styleUrls: ['./editar-usuario.component.scss'],
 })
 export class EditarUsuarioComponent implements OnInit {
-  editUserForm: FormGroup;
-  user!: any; // Almacenar toda la información del usuario
+  buscarUsuarioForm!: FormGroup;
+  usuarioForm!: FormGroup;
+  mostrarInformacionUsuario: boolean = false;
+  usuarioActual!: AuthCreateUserRequest;
 
-  constructor(private formBuilder: FormBuilder, 
-              private userService: AuthService) {
-    
-    this.editUserForm = this.formBuilder.group({
-      firstName: [''],       // Nombre
-      secondName: [''],      // Segundo Nombre
-      firstLastName: [''],   // Primer Apellido
-      secondLastName: [''],  // Segundo Apellido
-      phone: [''],           // Número de Teléfono
-    });
-  }
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private alertController: AlertController,
+    private route: ActivatedRoute,
+    private loadingController: LoadingController
+
+  ) { }
+
+
   
-  ngOnInit(): void {
-    this.loadUser();
-  }
+  ngOnInit() {
+    // Inicializar el formulario
+    this.usuarioForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      firstName: ['', Validators.required],
+      secondName: [''],
+      firstLastName: ['', Validators.required],
+      secondLastName: [''],
+      phone: ['+', [Validators.required, Validators.pattern(/^\+?[0-9]*$/)]],
+      roleListName: ['', Validators.required],
+      isActivated: [''],
+      changePassword: [false],
+      passwordNew: ['']
+    });
 
-  loadUser() {
-    const email = this.userService.getEmailFromToken();
-    if (email) {
-      this.userService.searchByEmail(email).subscribe(
-        (user) => {
-          this.user = user; // Aquí almacenas toda la información del usuario
-          this.editUserForm.patchValue({
-            firstName: user.firstName,
-            secondName: user.secondName,
-            firstLastName: user.firstLastName,
-            secondLastName: user.secondLastName,
-            phone: user.phone,
-          });
-        },
-        (error) => {
-          console.error('Error fetching user:', error);
-        }
-      );
+    // Obtener el ID del usuario desde la URL
+    const userId = this.route.snapshot.paramMap.get('id');
+    if (userId) {
+      this.buscarUsuarioPorId(+userId);  // Convierte el ID a número
     }
   }
 
-  onSubmit() {
-    if (this.editUserForm.valid) {
-        const userId = this.user.id; // Asegúrate de que el ID del usuario se haya cargado correctamente
-        const updatedUser = {
-            ...this.editUserForm.value, // Obtiene los valores del formulario
+  buscarUsuarioPorId(id: number) {
+    // Llamada al servicio para buscar el usuario por ID
+    this.authService.getUserById(id).subscribe(
+      (usuario) => {
+        if (usuario && usuario.role && usuario.role.roleEnum) {
+          this.usuarioActual = {
+            ...usuario,
+            id: usuario.id // Mapea 'id' a 'id_usuario'
+          };
+          
+          // Asignar valores al formulario, incluyendo el rol
+          this.usuarioForm.patchValue({
+            firstName: usuario.firstName,
+            secondName: usuario.secondName,
+            firstLastName: usuario.firstLastName,
+            secondLastName: usuario.secondLastName,
+            email: usuario.email,
+            phone: usuario.phone,
+            roleListName: usuario.role.roleEnum,  // Asignar el rol correctamente
+            isActivated: usuario.isActivated
+          });
+    
+          this.mostrarInformacionUsuario = true;
+        } else {
+          this.mostrarAlerta('Error en la respuesta', 'No se encontró un rol válido para el usuario.');
+        }
+      },
+      (error) => {
+        this.mostrarAlerta('Error', 'No se pudo obtener el usuario.');
+      }
+    );
+  }
+
+  onPhoneInput(event: any) {
+    const inputValue = event.target.value;
+  
+    // Verifica si el valor no comienza con '+'
+    if (!inputValue.startsWith('+')) {
+      event.target.value = '+' + inputValue.replace(/\D/g, ''); // Reinsertar el '+' y eliminar caracteres no numéricos
+    } else {
+      // Solo permite números después del '+'
+      event.target.value = '+' + inputValue.slice(1).replace(/\D/g, ''); // Permitir solo dígitos
+    }
+  
+    // Actualiza el valor en el formulario
+    this.usuarioForm.patchValue({ phone: event.target.value });
+  }
+
+
+
+  async actualizarUsuario() {
+    const id = this.usuarioActual?.id;
+  
+    if (this.usuarioForm.valid && id != undefined) {
+      const result = await this.presentConfirmUpdate();
+      
+      if (result) {
+        const loading = await this.loadingController.create({
+          message: 'Actualizando...', // Mensaje de carga
+          spinner: 'circles', // Tipo de spinner
+          duration: 5000 // Tiempo máximo antes de ocultar el loading, opcional
+        });
+        await loading.present();
+  
+        const nuevaData: AuthCreateUserRequest = {
+          firstName: this.usuarioForm.get('firstName')?.value,
+          secondName: this.usuarioForm.get('secondName')?.value,
+          firstLastName: this.usuarioForm.get('firstLastName')?.value,
+          secondLastName: this.usuarioForm.get('secondLastName')?.value,
+          email: this.usuarioForm.get('email')?.value,
+          password: this.usuarioForm.get('changePassword')?.value ? this.usuarioForm.get('passwordNew')?.value : '',
+          phone: this.usuarioForm.get('phone')?.value,
+          isActivated: this.usuarioForm.get('isActivated')?.value,
+          authCreateRoleRequest: {
+            roleListName: [this.usuarioForm.get('roleListName')?.value]
+          }
         };
 
-        this.userService.updateUserById(userId, updatedUser).subscribe(
-            (response) => {
-                console.log('Usuario actualizado con éxito:', response);
-                // Redirigir o mostrar un mensaje de éxito aquí
-            },
-            (error) => {
-                console.error('Error actualizando el usuario:', error);
-                if (error.status === 401) {
-                    alert('No estás autorizado para realizar esta acción. Por favor, inicia sesión nuevamente.');
-                    // Opcionalmente redirigir al inicio de sesión
-                } else {
-                    alert('Hubo un problema al actualizar el usuario. Inténtalo de nuevo.');
-                }
-            }
+        
+        this.authService.updateUserById(id, nuevaData).subscribe(
+          (response) => {
+            loading.dismiss(); // Ocultar la pantalla de carga
+            this.mostrarAlerta('Usuario actualizado', 'Usuario actualziado correctamente');
+            // Manejar la respuesta, como redirigir o mostrar un mensaje
+          },
+          (error) => {
+            loading.dismiss(); // Ocultar la pantalla de carga en caso de error
+            this.mostrarAlerta('Error al actualizar', "Ocurrio un error al actualizar intentelo nuevamente");
+            // Manejar el error, como mostrar un mensaje de error
+          }
         );
-    } else {
-        console.log('Formulario no válido');
-        // Mostrar mensajes de validación
+  
+        
+      }
     }
-}
+    
+  }
+
+  async presentConfirm(): Promise<boolean> {
+    return new Promise(async (resolve) => {
+      const alert = await this.alertController.create({
+        header: 'Confirmación',
+        message: '¿Está seguro de que desea desacticar a este usuario? ',
+        buttons: [
+          {
+            text: 'Cancelar',
+            role: 'cancel',
+            handler: () => {
+              resolve(false);
+            }
+          }, {
+            text: 'Desactivar',
+            handler: () => {
+              // Aquí puedes poner el código para eliminar el elemento
+              resolve(true);
+            }
+          }
+        ]
+      });
+
+      await alert.present();
+    });
+  }
+
+  async presentConfirmUpdate(): Promise<boolean> {
+    return new Promise(async (resolve) => {
+      const alert = await this.alertController.create({
+        header: 'Confirmación',
+        message: '¿Está seguro de que desea Actualizar  a este usuario? ',
+        buttons: [
+          {
+            text: 'Cancelar',
+            role: 'cancel',
+            handler: () => {
+              resolve(false);
+            }
+          }, {
+            text: 'Actualizar',
+            handler: () => {
+              // Aquí puedes poner el código para eliminar el elemento
+              resolve(true);
+            }
+          }
+        ]
+      });
+
+      await alert.present();
+    });
+  }
+
+
+  async mostrarAlerta(titulo: string, mensaje: string) {
+    const alert = await this.alertController.create({
+      header: titulo,
+      message: mensaje,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
+
+  toggleActivation() {
+    const currentValue = this.usuarioForm.get('isActivated')?.value;
+    this.usuarioForm.patchValue({ isActivated: !currentValue });
+  }
+
 
 }
