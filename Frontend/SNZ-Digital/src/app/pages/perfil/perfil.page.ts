708c03ca65bb103a027068e90bde7871418e76f6
NodyDuoc <101402582+NodyDuoc@users.noreply.github.com>
2024-10-28 18:36:27 -0300

    Eliminar metodos innecesarios

--- Frontend/SNZ-Digital/src/app/pages/perfil/perfil.page.ts
+++ Frontend/SNZ-Digital/src/app/pages/perfil/perfil.page.ts
@@ -36,7 +36,6 @@ export class PerfilPage implements OnInit {
 
 
   ngOnInit(): void {
-    this.loadProductos();
     this.loadUser();
 
   }
@@ -46,44 +45,6 @@ ionViewWillEnter() {
   this.loadUser(); // Cargar usuario cada vez que la vista entra
 }
   
-  loadProductos(): void {
-    this.categoriaService.getCategorias().subscribe({
-      next: (data: Categoria[]) => {
-        this.categorias = data;
-        console.log('Categorías:', this.categorias);
-        
-        this.categoriasIds = [];
-        this.categorias.forEach(categoria => {
-          console.log('Cargando productos para la categoría:', categoria);
-          this.categoriasIds.push(categoria.catId);
-          
-          this.productoService.getProductosByCategoria(categoria.catId).subscribe({
-            next: (productos) => {
-              console.log(`Productos para la categoría ${categoria.catId}:`, productos);
-              this.productosPorCategoria[categoria.catId] = productos;
-            },
-            error: (err) => {
-              console.error(`Error al cargar los productos de la categoría ${categoria.catId}:`, err);
-            }
-          });
-        });
-      },
-      error: (error) => {
-        console.error('Error al obtener las categorías:', error);
-      }
-    });
-  }
-
-agregarAlCarrito(producto: Producto) {
-    // Lógica para agregar el producto al carrito
-    console.log('Producto agregado al carrito:', producto);
-}
-
-comprarAhora(producto: Producto) {
-    // Lógica para realizar la compra del producto
-    console.log('Iniciar compra para el producto:', producto);
-}
-
 // Funciones propias
 
 loadUser() {
