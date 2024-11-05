77bc44bf0e29cad6cda4ba543a23d64527c4a85b
NodyDuoc <101402582+NodyDuoc@users.noreply.github.com>
2024-11-05 13:33:49 -0300

    Valoraciones funcionales

---
+++ Frontend/SNZ-Digital/src/models/valoracion.ts
@@ -0,0 +1,9 @@
+// src/app/models/Valoracion.model.ts
+export interface Valoracion {
+    valid: number;
+    valComentario: string;
+    valPuntuacion: number;
+    usuariosUserId: number;
+    productoProductId: number;
+  }
+  
