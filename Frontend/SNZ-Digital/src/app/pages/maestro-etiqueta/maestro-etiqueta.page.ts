21de681e77494704667b537ebd9e1ea2d88a20d3
NodyDuoc <101402582+NodyDuoc@users.noreply.github.com>
2024-11-06 21:22:40 -0300

    Correccion models

--- Frontend/SNZ-Digital/src/app/pages/maestro-etiqueta/maestro-etiqueta.page.ts
+++ Frontend/SNZ-Digital/src/app/pages/maestro-etiqueta/maestro-etiqueta.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { EtiquetaService } from 'src/app/Service/EtiquetaService.service';
+import { Etiqueta } from 'src/models/etiqueta';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-maestro-etiqueta',
@@ -6,10 +9,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./maestro-etiqueta.page.scss'],
 })
 export class MaestroEtiquetaPage implements OnInit {
+  etiquetas: Etiqueta[] = [];
+  errorMessage: string = '';
+  toastMessage: string | null = null;
+  toastColor: string = 'success';
 
-  constructor() { }
+  constructor(
+    private etiquetaService: EtiquetaService,
+    private toastController: ToastController
+  ) {}
 
   ngOnInit() {
+    this.cargarEtiquetas();
   }
 
+  cargarEtiquetas() {
+    this.etiquetaService.getAllEtiquetas().subscribe(
+      (data: Etiqueta[]) => {
+        this.etiquetas = data;
+        if (this.etiquetas.length === 0) {
+          this.errorMessage = 'No hay etiquetas disponibles.';
+        }
+      },
+      (error) => {
+        console.error('Error al obtener las etiquetas', error);
+        this.errorMessage = 'Hubo un problema al cargar las etiquetas. Por favor, intenta más tarde.';
+      }
+    );
+  }
+
+  async showToast(message: string, color: string = 'success') {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 3000,
+      color: color,
+      position: 'top'
+    });
+    toast.present();
+  }
 }
---
+++ Frontend/SNZ-Digital/src/models/etiqueta.ts
@@ -0,0 +1,5 @@
+export interface Etiqueta {
+    etiquetaId: number;  // ID único para la etiqueta
+    nombreEtiqueta: string;      // Nombre de la etiqueta
+    detalleEtiqueta: string; // Descripción de la etiqueta (si es necesario)
+}
